623f0b66cad093aeccdde7b725bd22a4088f16da
chanxinguidao <693196756@qq.com>
2019-05-25 09:21:49 +0800

    负载均衡 ribbon实现

--- chapter2/service-ribbon/src/main/java/com/example/serviceribbon/controller/HelloController.java
+++ chapter2/service-ribbon/src/main/java/com/example/serviceribbon/controller/HelloController.java
@@ -1,4 +1,19 @@
 package com.example.serviceribbon.controller;
 
+import com.example.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping
+    public String hello(@RequestParam String name){
+        return helloService.hello(name);
+    }
 }
